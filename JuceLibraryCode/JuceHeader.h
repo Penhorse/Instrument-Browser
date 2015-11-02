28844113d160f26f47992b1c21e2f3c8d29737cc
Chris Penrose <chris@pleasesendhelp.com>
2015-11-02 14:32:58 +0000

    project name

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -34,7 +34,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "InstrumentBrowser";
+    const char* const  projectName    = "Instrument Browser";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
