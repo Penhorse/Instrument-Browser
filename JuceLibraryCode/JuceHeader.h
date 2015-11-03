1522c5c1f8cd8c5a3d05c4e701bf3a18c829c14d
Chris Penrose <chris@pleasesendhelp.com>
2015-11-03 19:45:04 +0000

    updated version to 1.1.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -35,8 +35,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Instrument Browser";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.1.0";
+    const int          versionNumber  = 0x10100;
 }
 #endif
 
